489a67ab201d58d2d21594db6cda4d6555339ac6
Alexis Armin Huf <alexishuf@gmail.com>
2016-04-11 22:29:47 -0300

    pagamaento x região absoluto (questão 11)

--- script.R
+++ script.R
@@ -154,7 +154,7 @@ par(mar=c(7, 4, 4, 2) + 0.1, mgp=c(3, 1, 0))
 barplot(x, xpd=TRUE, xlab = "", ylab = "Alunos", beside = FALSE, legend.text = FALSE, horiz = FALSE, ylim=c(0, 2000), las=2, 
         names.arg = c("Adm.", "Comp. e Mat.", "Educ.", "Eng. e Prod.", "Hum.", "Júr. e Cont."))
 legend("topleft", c("Muito insatisfeito", "Insatisfeito", "Indiferente", "Satisfeito", "Muito satisfeito"),  bty="n", fill=gray.colors(5))
-mtext("Área", side=2, line = 6.5, cex=1)
+mtext("Área", side=1, line = 6, cex=1)
 par(mar=c(5, 4, 4, 2) + 0.1, mgp=c(3, 1, 0))
 dev.off()
 
@@ -176,9 +176,11 @@ dev.off()
 x = table(df$Pagamento,df$Região)
 
 setEPS()
-postscript("plots/stacked_pagamento_por_regiao.eps", fonts = c("serif", "Palatino"))
-barplot(x, xlab = "Região", ylab = "Alunos", beside = FALSE, family = "serif", font=1, cex.axis = 1.1, cex.names = 1.1, cex.lab=1.1)
-legend("topright", rownames(x),  bty="n", fill=gray.colors(length(rownames(x))),  text.font=1, cex = 1.0)
+postscript("plots/stacked_pagamento_por_regiao.eps", fonts = c("serif", "Palatino"), family="serif", pointsize = 12, pagecentre=TRUE, width=4.95, height=4.95)
+barplot(x, xlab = "", ylab = "Alunos", beside = FALSE, ylim = c(0, 2000), las=2, 
+        names.arg = abbrvRegiao)
+legend("topright", rownames(x),  bty="n", fill=gray.colors(length(rownames(x))))
+mtext("Área", side=1, line=5.75)
 dev.off()
 
 #-------------------------------------------------------------------------------------------------
