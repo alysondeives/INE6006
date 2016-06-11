78d0d0c3c06dbc0f27d2a4dad18f6b63f448b647
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-06-11 17:37:43 -0300

    questao 2c

---
+++ lista3/questao2/powers.gp
@@ -0,0 +1,17 @@
+set datafile separator ";"
+
+set terminal postscript eps enhanced color
+set output "powers.eps"
+
+set xrange [3.50:4.35]
+set yrange [0.0:1.0]
+#set xtics 3.50,0.05,4.35
+#set ytics 0,0.1,1
+set key on inside right top 
+set xlabel "Media"
+set ylabel "Powder do Teste"
+
+plot 'powers.csv' using 1:2 title "40" with linespoints lt 1, \
+     'powers.csv' using 1:3 title "50" with linespoints lt 2, \
+     'powers.csv' using 1:4 title "60" with linespoints lt 3, \
+     'powers.csv' using 1:5 title "70" with linespoints lt 4
--- lista3/questao2/script.R
+++ lista3/questao2/script.R
@@ -34,3 +34,25 @@ print(sample20powers$power)
 #==============================================================================
 #                        Análise do Poder do Teste
 #==============================================================================
+
+medias <- c(seq(3.50, 4.35, by=0.05))
+deltas <- c((H0 - medias)/sample20s)
+
+sample40powers <- power.t.test(n=40, d=deltas, sd=sample20s, sig.level=0.01, power=NULL, type="one.sample", alternative="one.sided", strict=FALSE)
+sample50powers <- power.t.test(n=50, d=deltas, sd=sample20s, sig.level=0.01, power=NULL, type="one.sample", alternative="one.sided", strict=FALSE)
+sample60powers <- power.t.test(n=60, d=deltas, sd=sample20s, sig.level=0.01, power=NULL, type="one.sample", alternative="one.sided", strict=FALSE)
+sample70powers <- power.t.test(n=70, d=deltas, sd=sample20s, sig.level=0.01, power=NULL, type="one.sample", alternative="one.sided", strict=FALSE)
+
+print(sample40powers$power)
+print(sample50powers$power)
+print(sample60powers$power)
+print(sample70powers$power)
+
+powers <- matrix(c(medias, sample40powers$power, sample50powers$power, sample60powers$power, sample70powers$power), nrow=length(deltas), ncol=5)
+colnames(powers) <- c("media", "40", "50", "60", "70")
+print(powers)
+
+write.csv(powers, file = "powers.csv", row.names = FALSE, 
+          na="", fileEncoding = "UTF-8", eol="\r\n")
+
+
