b56e1a96182d82149248e4859ed18a1ba01582f9
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-06-16 09:55:02 -0300

    usando listings

--- lista3/questao1/b.R
+++ lista3/questao1/b.R
@@ -1,4 +1,8 @@
-us1b <- c(28, 30, 31, 32, 33, 34, 35)
+us1b     <- c(28, 30, 31, 32, 33, 34, 35)
 deltas1b <- us1b-u01a
-bpower <- power.t.test(n=n1a, delta=deltas1b, sd=s1a, sig.level=alpha1a, 
-                       type="one.sample", alternative="one.sided")
+bpower   <- power.t.test(n           = n1a,
+                         delta       = deltas1b, 
+                         sd          = s1a,
+                         sig.level   = alpha1a, 
+                         type        = "one.sample",
+                         alternative = "one.sided")
