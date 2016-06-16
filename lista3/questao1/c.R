7381bd91f2489752aff54b11bb0536d58dff1bd3
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-06-16 10:03:57 -0300

    questao 1.c

--- lista3/questao1/c.R
+++ lista3/questao1/c.R
@@ -1,8 +1,19 @@
-cpower <- power.t.test(delta=abs(29-u01a), sd=s1a, sig.level=0.05, power = 0.9,
-                       type="one.sample", alternative="one.sided")
-n1c <- cpower$n
+cpower <- power.t.test(delta       = abs(29-u01a),
+                       sd          = s1a,
+                       sig.level   = 0.05,
+                       power       = 0.9,
+                       type        = "one.sample",
+                       alternative ="one.sided")
+
+n1c    <- cpower$n
+
 nMin1c <- ceiling(n1c)
 
-cpower <- power.t.test(n=nMin1c, delta=abs(29-u01a), sd=s1a, sig.level=0.05,
-                       type="one.sample", alternative="one.sided")
+cpower <- power.t.test(n           = nMin1c,
+                       delta       = abs(29-u01a),
+                       sd          = s1a,
+                       sig.level   = 0.05,
+                       type        = "one.sample",
+                       alternative = "one.sided")
+
 beta1c <- cpower$power
