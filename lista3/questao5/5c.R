6e240eea89076783558ee90c25573a914a69eca4
eliza <elizahelena.gomes@gmail.com>
2016-06-18 14:19:52 -0300

    questao5-2

---
+++ lista3/questao5/5c.R
@@ -0,0 +1,7 @@
+tempD <- (3)/1.0631
+pwr.t.test(n = NULL, 
+           d = tempD, 
+           sig.level = 0.01, 
+           power = 0.95, 
+           type = "two.sample", 
+           alternative = "two.sided")
