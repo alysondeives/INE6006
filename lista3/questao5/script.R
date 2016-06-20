c5db984b6179e626e94259fe196321ba1916029e
elizagomes <elizahelena.gomes@gmail.com>
2016-06-20 10:49:18 -0300

    questao 5 final

--- lista3/questao5/script.R
+++ lista3/questao5/script.R
@@ -9,6 +9,7 @@ amostra <- read.csv(file = "Questão5.csv", na.strings = c("NA",""," "), header=
 levels(amostra$Pagamento.C)
 mean(amostra$Renda[which(amostra$Pagamento.C=="Incentivos federais")])
 mean(amostra$Renda[which(amostra$Pagamento.C=="Outras Formas de Pagamento")])
+rowSums(amostra[which(amostra$Pagamento.C=="Outras Formas de Pagamento")])
 rowSums(amostra$Renda[which(amostra$Pagamento.C=="Incentivos federais")])
 sd(amostra$Renda[which(amostra$Pagamento.C=="Incentivos federais")])
 sd(amostra$Renda[which(amostra$Pagamento.C=="Outras Formas de Pagamento")])
@@ -21,3 +22,7 @@ var.test(incentivos$Renda, outras$Renda, alternative = "two.sided", conf.level =
 tempD <- (3)/1.0631
 pwr.t.test(n = NULL, d = tempD, sig.level = 0.01, power = 0.95, type = "two.sample", alternative = "two.sided")
 power.t.test(n = NULL, delta = 3, sd = 1.0631, sig.level = 0.01,power = 0.95, type = "two.sample",alternative = "two.sided",strict = TRUE)
+pwr.t2n.test(n1 = 28, n2 = 52, d = tempD, sig.level = 0.05, power = NULL, alternative = "two.sided")
+pwr.t2n.test(n1 = NULL, n2 = 52, d = tempD, sig.level = 0.01, power = 0.95, alternative = "two.sided")
+
+power.t.test(n = 80, delta = 1, sd = 1.0631, sig.level = 0.05, power = NULL, type = "two.sample", alternative = "two.sided")
